d946e2c7de4fff09dbeb5758c9b5b40badfe09ba
Turagats <o.napetvaridze@gmail.com>
2023-04-26 19:38:23 +0400

    TC

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,25 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Footer.scss";
+import tccomponent from "./SeasonComponents/TCobject";
 import game1 from "../Assets/game1.jpeg";
 import game2 from "../Assets/game2.jpeg";
 import game3 from "../Assets/game3.jpeg";
 import game4 from "../Assets/game4.jpeg";
 import game5 from "../Assets/game5.jpeg";
+import dropdown from "../Assets/dropdown.png";
 
 export default function Footer() {
+  const [terms, setTerms] = useState(tccomponent);
+
+  const toggleDetails = (index) => {
+    setTerms(
+      terms.map((term, i) =>
+        i === index
+          ? { ...term, showDetails: !term.showDetails, rotated: !term.rotated }
+          : term
+      )
+    );
+  };
+
   return (
     <div className="footer-container">
-      {}
       <div className="footer-section-1">
         <div className="footer-section-title">წესები და პირობები</div>
         <div className="footer-TC">
-          <div className="footer-TC-list">
-            როდის იწყება და რა ფორმატით გაიმართება აქცია
-          </div>
-          <div className="footer-TC-list">
-            როგორ მივიღო აქციაში მონაწილეობა?
-          </div>
-          <div className="footer-TC-list">სხვადასვა</div>
+          {terms.map((term, index) => (
+            <div key={index}>
+              <div
+                className="footer-TC-list"
+                onClick={() => toggleDetails(index)}
+              >
+                {term.title}
+                <img
+                  src={dropdown}
+                  alt=""
+                  style={{
+                    transform: term.rotated && "rotate(180deg)",
+                    transition: "transform 0.4s ease-in-out",
+                  }}
+                />
+              </div>
+              {term.showDetails && (
+                <div
+                  className="footer-TC-list-description"
+                  id="footer-TC-list-description"
+                  style={{
+                    color: "aliceblue",
+                    opacity: "0.7",
+                    transition: term.showDetails && "opacity 0.8s ease-in-out",
+                  }}
+                >
+                  {term.details}
+                </div>
+              )}
+            </div>
+          ))}
         </div>
       </div>
       <div className="footer-section-2">
